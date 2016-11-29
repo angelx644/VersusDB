a645514fb27d87463e026c9cf1e63a88d01d3765
cstei029 <cstei029@fiu.edu>
2016-11-28 22:46:37 -0500

    added missing pages

--- Meteor/VersusDB/client/index.js
+++ Meteor/VersusDB/client/index.js
@@ -7,7 +7,15 @@ import '../imports/ui/matches.html';
 import '../imports/ui/teams.html';
 import '../imports/ui/profile.html';
 import '../imports/ui/about.html';
-
+import '../imports/ui/ranked.html';
+import '../imports/ui/quick.html';
+import '../imports/ui/pendingranked.html';
+import '../imports/ui/rankedrequest.html';
+import '../imports/ui/rankedaccept.html';
+import '../imports/ui/creatematch.html';
+import '../imports/ui/pendingteam.html';
+import '../imports/ui/teamcreate.html';
+import '../imports/ui/teamjoinreqs.html';
 
 Meteor.startup(function () {
   Session.setDefault("templateName", "login")
@@ -34,5 +42,32 @@ Template.body.events({
   },
   "click .about": function() {
      Session.set("templateName", "about");
+  },
+  "click .ranked": function() {
+     Session.set("templateName", "ranked");
+  },
+  "click .quick": function() {
+     Session.set("templateName", "quick");
+  },
+  "click .pendingranked": function() {
+     Session.set("templateName", "pendingranked");
+  },
+  "click .rankedrequest": function() {
+     Session.set("templateName", "rankedrequest");
+  },
+  "click .rankedaccept": function() {
+     Session.set("templateName", "rankedaccept");
+  },
+  "click .creatematch": function() {
+     Session.set("templateName", "creatematch");
+  },
+  "click .pendingteam": function() {
+     Session.set("templateName", "pendingteam");
+  },
+  "click .teamcreate": function() {
+     Session.set("templateName", "teamcreate");
+  },
+  "click .teamjoinreqs": function() {
+     Session.set("templateName", "teamjoinreqs");
   }
 });
