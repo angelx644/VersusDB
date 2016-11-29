129ffa9fe100f0ac2585d47085795d221a88e7f9
cstei029 <cstei029@fiu.edu>
2016-11-28 23:17:34 -0500

    added databases

--- Meteor/VersusDB/server/main.js
+++ Meteor/VersusDB/server/main.js
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
-import '../imports/api/tasks.js';
+import '../imports/api/accounts.js';
+import '../imports/api/matches.js';
+import '../imports/api/requests.js';
+import '../imports/api/teams.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
